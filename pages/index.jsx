b15f494ba5fa184f789c35d374df748e5b399962
Luke Taylor <luke@deentaylor.com>
2019-11-05 21:54:32 -0500

    Add dynamic page for projects

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Index = () => (
   <div>
     <h1>Luke Deen Taylor</h1>
+    <Link href="/work/[project]" as="work/codus"><a>Codus</a></Link>
   </div>
 );
 
---
+++ pages/work/[project].jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ProjectDetails = () => {
+  const router = useRouter();
+
+  return (
+    <h1>
+      Work:&nbsp;
+      {router.query.project}
+    </h1>
+  );
+};
+
+export default ProjectDetails;
