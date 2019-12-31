4f2e82dc767c07107f9c7220c4a0a6e791469e72
Luke Taylor <luke@deentaylor.com>
2019-12-31 13:23:16 -0500

    Add config for styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 
+import styles from './index.css';
+
 const Index = () => (
   <div>
-    <h1>Luke Deen Taylor</h1>
+    <h1 className="heading-main">Luke Deen Taylor</h1>
     <Link href="/work/[project]" as="work/codus"><a>Codus</a></Link>
+
+    <style jsx>{styles}</style>
   </div>
 );
 
