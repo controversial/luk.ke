ef4d96eafde49b4c6c3e8d8b0405910e304e6909
Luke Taylor <luke@deentaylor.com>
2020-01-01 00:36:49 -0600

    Clearer orientation class names for TwoPanels component

--- components/TwoPanels.jsx
+++ components/TwoPanels.jsx
@@ -14,8 +14,14 @@ class TwoPanels extends React.Component {
 
   render() {
     const { children, orientation } = this.props;
+    const orientationClass = ({
+      left: 'panel-on-left',
+      right: 'panel-on-right',
+      full: 'panel-full',
+    })[orientation];
+
     return (
-      <div className={`panel-layout panel-${orientation}`}>
+      <div className={`panel-layout ${orientationClass}`}>
         <div className="panels-background">
           <div className="panel-bg light" />
           <div className="panel-bg dark" />
