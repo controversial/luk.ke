52329d9f3adfe41fc53a2e69dd7d1248812ceef4
Luke Taylor <luke@deentaylor.com>
2020-01-06 12:10:07 -0600

    Allow global styles

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
 module.exports = {
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: require('styled-jsx/webpack').loader,
-          options: {
-            type: 'scoped',
-          },
-        },
-      ],
-    });
-
-    config.module.rules.push({
       test: /\.sass$/,
       use: [
         defaultLoaders.babel,
         {
           loader: require('styled-jsx/webpack').loader,
           options: {
-            type: 'scoped',
+            type: (fileName, options) => options.query.type || 'scoped',
           },
         },
       ],
