37063af228eae231c984158bd5b110bdcdb141af
Luke Taylor <luke@deentaylor.com>
2020-01-06 22:53:27 -0600

    Use PanelsContent on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PanelsContent from '../components/PanelsLayout/PanelsContent.jsx';
+
 import styles from './index.sass';
 
 class Index extends React.Component {
@@ -8,9 +10,20 @@ class Index extends React.Component {
   render() {
     return (
       <div className="page home">
-        <div className="left">
-          <h1 className="heading-main">Luke Deen Taylor</h1>
-        </div>
+
+        <PanelsContent
+          orientation={Index.panelOrientation}
+          darkContent={(
+            <div>
+              <h1 className="heading-main">Hi, I&rsquo;m Luke!</h1>
+            </div>
+          )}
+          lightContent={(
+            <div>
+              <div className="gradient" />
+            </div>
+          )}
+        />
 
         <style jsx>{styles}</style>
       </div>
