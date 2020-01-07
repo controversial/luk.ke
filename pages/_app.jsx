9478624c125ebabba6871e5abb2a6f14ae7a0744
Luke Taylor <luke@deentaylor.com>
2020-01-07 12:18:37 -0600

    Make app.js a functional component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import DefaultApp from 'next/app';
+import PropTypes from 'prop-types';
 import Router from 'next/router';
 
 import PanelsLayout from '../components/PanelsLayout/PanelsLayout.jsx';
 
 import baseStyles from '../styles/base.sass?type=global';
 
-class App extends DefaultApp {
-  render() {
-    const { Component, pageProps } = this.props;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <PanelsLayout orientation={Component.panelOrientation}>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </PanelsLayout>
 
-    return (
-      <>
-        <PanelsLayout orientation={Component.panelOrientation}>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </PanelsLayout>
-
-        <style jsx>{baseStyles}</style>
-      </>
-    );
-  }
+      <style jsx>{baseStyles}</style>
+    </>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
+App.defaultProps = { pageProps: {} };
+
 
 export default App;
 
