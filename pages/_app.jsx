67ab0307fd35511dd074ed6c85005c72c091f9b0
Luke Taylor <luke@deentaylor.com>
2020-01-07 12:30:56 -0600

    Add NavBar to App component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 
+import NavBar from '../components/NavBar/NavBar.jsx';
 import PanelsLayout from '../components/PanelsLayout/PanelsLayout.jsx';
 
 import baseStyles from '../styles/base.sass?type=global';
 
 function App({ Component, pageProps }) {
+  const { panelOrientation, pageName } = Component;
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
+    <div id="app">
+      <NavBar
+        currPageName={pageName}
+        orientation={panelOrientation}
+        panelWidth="40vw"
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+      />
+
       <PanelsLayout orientation={Component.panelOrientation}>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </PanelsLayout>
 
       <style jsx>{baseStyles}</style>
-    </>
+    </div>
   );
 }
 
