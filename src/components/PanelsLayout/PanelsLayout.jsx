a8abf8a76e34464410e35bde1c9a0b117fe088d6
Luke Taylor <luke@deentaylor.com>
2020-01-08 06:59:01 -0600

    Rewrite PanelsLayout as functional component

--- src/components/PanelsLayout/PanelsLayout.jsx
+++ src/components/PanelsLayout/PanelsLayout.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -22,34 +20,31 @@ export function getOrientationClass(orientation) {
  * opposite side of the fixed light panel from the scrolling dark panel.
  */
 
-class PanelsLayout extends React.Component {
-  static propTypes = {
-    children: PropTypes.element.isRequired,
-    orientation: PropTypes.oneOf(['left', 'right', 'full']).isRequired,
-    menuOpen: PropTypes.bool.isRequired,
-  }
+function PanelsLayout({ children, orientation, menuOpen }) {
+  const orientationClass = getOrientationClass(orientation);
 
-  render() {
-    const { children, orientation, menuOpen } = this.props;
-    const orientationClass = getOrientationClass(orientation);
-
-    return (
-      <div className={`panels-layout ${orientationClass} ${menuOpen ? 'menu-open' : ''}`}>
-        <div className="panels-background">
-          <div className="panel-bg light" />
-          <div className="panel-bg dark" />
-        </div>
-
-        <div className="panels-content">
-          { React.Children.only(children) }
-        </div>
-
-        <Menu orientation={orientation} />
-
-        <style jsx>{ styles }</style>
+  return (
+    <div className={`panels-layout ${orientationClass} ${menuOpen ? 'menu-open' : ''}`}>
+      <div className="panels-background">
+        <div className="panel-bg light" />
+        <div className="panel-bg dark" />
       </div>
-    );
-  }
+
+      <div className="panels-content">
+        { React.Children.only(children) }
+      </div>
+
+      <Menu orientation={orientation} />
+
+      <style jsx>{ styles }</style>
+    </div>
+  );
 }
+
+PanelsLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+  orientation: PropTypes.oneOf(['left', 'right', 'full']).isRequired,
+  menuOpen: PropTypes.bool.isRequired,
+};
 
 export default PanelsLayout;
