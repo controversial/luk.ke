064863d57187e4cf1d8c7fcf1d58bc38ffc4a137
Luke Taylor <luke@deentaylor.com>
2020-01-12 16:25:18 -0500

    Animate menu icon to X when menu opens

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,48 @@ import { getOrientationClass } from '../PanelsLayout/PanelsLayout.jsx';
 
 import styles from './NavBar.sass';
 
+
+function icon(menuOpen) {
+  // const transition = {
+  //   type: 'tween',
+  //   duration: 0.35,
+  //   ease: 'backIn',
+  // };
+  const transition = {
+    type: 'spring',
+    mass: 0.25,
+    stiffness: 150,
+    damping: 10,
+    velocity: 0,
+  };
+
+  return (
+    <div className="icon">
+      <motion.div
+        className="line"
+        animate={{
+          width: menuOpen ? '90%' : '100%',
+          rotate: menuOpen ? 45 : 0,
+          y: menuOpen ? 0 : '-.5em',
+        }}
+        initial={false}
+        transition={transition}
+      />
+      <motion.div
+        className="line"
+        animate={{
+          width: menuOpen ? '90%' : '100%',
+          rotate: menuOpen ? -45 : 0,
+          y: menuOpen ? 0 : '.5em',
+        }}
+        initial={false}
+        transition={transition}
+      />
+    </div>
+  );
+}
+
+
 function NavBar({ orientation, panelWidth, currPageName }) {
   const { state: { menuOpen }, dispatch } = useStore();
 
@@ -21,16 +63,7 @@ function NavBar({ orientation, panelWidth, currPageName }) {
         className="menu-button"
         onClick={() => { dispatch('setMenuOpen', !menuOpen); }}
       >
-        <div className="icon">
-          <motion.div
-            className="line"
-            animate={{ y: '-.5em' }}
-          />
-          <motion.div
-            className="line"
-            animate={{ y: '.5em' }}
-          />
-        </div>
+        { icon(menuOpen) }
         <div className="label">{menuOpen ? 'Close' : currPageName}</div>
       </button>
 
