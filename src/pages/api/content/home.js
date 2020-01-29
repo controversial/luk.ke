412164983976d0a5b9caa37476fab73adffe52db
Luke Taylor <luke@deentaylor.com>
2020-01-28 22:16:40 -0500

    Split business logic of API routes apart from handlers

--- src/pages/api/content/home.js
+++ src/pages/api/content/home.js
@@ -2,15 +2,19 @@ import Prismic from 'prismic-javascript';
 import PrismicDOM from 'prismic-dom';
 import initApi from '../../../helpers/prismic';
 
-export default async (req, res) => {
+export async function getHomepage(req) {
   const api = await initApi(req);
   /* eslint-disable camelcase */
-  const homepage = await api.query(Prismic.Predicates.at('document.type', 'homepage'))
+  return api.query(Prismic.Predicates.at('document.type', 'homepage'))
     .then(({ results }) => results[0].data)
     .then(({ main_title, text }) => ({
       title: PrismicDOM.RichText.asHtml(main_title),
       text: text.map(({ text_content }) => PrismicDOM.RichText.asHtml(text_content)),
     }));
   /* eslint-enable */
+}
+
+export default async (req, res) => {
+  const homepage = await getHomepage(req);
   res.status(200).json(homepage);
 };
