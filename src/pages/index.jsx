b41701c9010782169ddab8130ffe4691d90cf728
Luke Taylor <luke@deentaylor.com>
2020-01-28 22:09:25 -0500

    Rewrite index page as functional component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,33 +4,32 @@ import PanelsContent from '../components/PanelsLayout/PanelsContent.jsx';
 
 import styles from './index.sass';
 
-class Index extends React.Component {
-  static pageName = 'Hello';
+function Index() {
+  return (
+    <div className="page home">
 
-  static panelOrientation = 'right';
+      <PanelsContent
+        orientation={Index.panelOrientation}
+        darkContent={(
+          <div>
+            <h1 className="heading-main">Hi, I&rsquo;m Luke!</h1>
+          </div>
+        )}
+        lightContent={(
+          <div>
+            <div className="gradient" />
+          </div>
+        )}
+      />
 
-  render() {
-    return (
-      <div className="page home">
-
-        <PanelsContent
-          orientation={Index.panelOrientation}
-          darkContent={(
-            <div>
-              <h1 className="heading-main">Hi, I&rsquo;m Luke!</h1>
-            </div>
-          )}
-          lightContent={(
-            <div>
-              <div className="gradient" />
-            </div>
-          )}
-        />
-
-        <style jsx>{styles}</style>
-      </div>
-    );
-  }
+      <style jsx>{styles}</style>
+    </div>
+  );
 }
+
+Object.assign(Index, {
+  pageName: 'Hello',
+  panelOrientation: 'right',
+});
 
 export default Index;
