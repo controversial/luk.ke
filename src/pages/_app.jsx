ca810ed45e17b167efc003cee4a20715150c3ebf
Luke Taylor <luke@deentaylor.com>
2020-03-21 02:18:14 -0400

    Adapt _app.jsx to support new layout scheme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,26 +9,35 @@ import PanelsLayout from '../components/PanelsLayout/PanelsLayout.jsx';
 import baseStyles from '../styles/base.sass?type=global';
 
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps: basePageProps }) {
   const { panelOrientation, pageName: initialPageName } = Component;
 
   const [pageName, setPageName] = useState(initialPageName);
   Router.events.on('routeChangeComplete', () => { setPageName(Component.pageName); });
 
+  const pageProps = { ...basePageProps, setPageName };
+
   return (
     <StoreProvider>
       <div id="app">
         <PanelsLayout
-          orientation={panelOrientation}
+          // Well-behaved page components should define LightContent and DarkContent as properties,
+          // but pages that don't do this will still be rendered as full-screen "light" content
+          lightContent={React.createElement(Component.LightContent || Component, pageProps)}
+          darkContent={React.createElement(Component.DarkContent || React.Fragment, pageProps)}
+          // If the component does not provide both light content and dark content, force the "full"
+          // layout.
+          orientation={(Component.LightContent && Component.DarkContent) ? panelOrientation : 'full'}
           currPageName={pageName}
-        >
-          {/* eslint-disable react/jsx-props-no-spreading */}
-          <Component
-            {...pageProps}
-            setPageName={setPageName}
-          />
-          {/* eslint-enable */}
-        </PanelsLayout>
+        />
+
+        {/*
+          If the page component provides LightContent, then the main Component won't get rendered in
+          PanelsLayout. Whenever this is the case, we elect to render in the main app container
+          instead. In most cases, it won't render real DOM content.
+        */}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        { Component.LightContent ? <Component {...pageProps} /> : '' }
 
         <style jsx>{baseStyles}</style>
       </div>
