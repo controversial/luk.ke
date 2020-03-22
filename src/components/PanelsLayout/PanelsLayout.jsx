2c39abf4e11eb92bbcd336b3ec9f44b200d2cb8e
Luke Taylor <luke@deentaylor.com>
2020-03-22 16:17:57 -0400

    Add basic menu toggle button back

--- src/components/PanelsLayout/PanelsLayout.jsx
+++ src/components/PanelsLayout/PanelsLayout.jsx
@@ -4,6 +4,7 @@ import { useStore } from '../../store';
 
 import { motion } from 'framer-motion';
 import Menu from '../Menu/Menu.jsx';
+import MenuIcon from '../NavBar/MenuIcon.jsx';
 
 import getOrientationClass from '../../helpers/getOrientationClass';
 
@@ -20,54 +21,76 @@ function PanelsLayout({ lightContent, darkContent, orientation, currPageName }) 
   const { state: { menuOpen, dimensions }, dispatch } = useStore();
   const orientationClass = getOrientationClass(orientation);
 
+  const variant = menuOpen ? 'menu-open' : 'menu-closed';
+
   return (
-    <motion.div
-      className={`${styles.panelsLayout} ${styles[orientationClass]} ${menuOpen ? styles.menuOpen : ''}`}
-      variants={{
-        'menu-open': (orient) => ({ x: orient === 'right' ? `-${dimensions.menuWidth}` : dimensions.menuWidth }),
-        'menu-closed': { x: 0 },
-      }}
-      animate={menuOpen ? 'menu-open' : 'menu-closed'}
-      transition={{ type: 'tween', duration: 0.5, ease: [0.25, 0.1, 0.25, 1] }}
-      custom={orientation}
-      initial={false}
-    >
-      {/* There's a menu off screen to the left */}
-      <Menu orientation="left" />
+    <React.Fragment>
+      {/* Menu button that floats above */}
+      <div className={`${styles.menuButton} ${styles[orientationClass]}`}>
+        <motion.button
+          type="button"
+          onClick={() => { dispatch('setMenuOpen', !menuOpen); }}
+          animate={variant}
+          initial={false}
+        >
+          <MenuIcon />
+          <div className={styles.label}>{ currPageName }</div>
+        </motion.button>
+      </div>
 
-      {/* Light panel */}
+      {/*
+        The main panels container. Displays a dark and light panel that fill up the whole viewport,
+        as well as a menu to the left and the right. This whole container shifts to the left/right
+        to reveal the menu.
+      */}
       <motion.div
-        layoutTransition
-        className={`${styles.panel} ${styles.light}`}
-        style={{ gridColumn: {
-          left: 'viewport-left / fifth 2',
-          right: 'fifth 3 / viewport-right',
-          full: 'viewport-left / viewport-right',
-        }[orientation] }}
+        className={`${styles.panelsLayout} ${styles[orientationClass]} ${menuOpen ? styles.menuOpen : ''}`}
+        variants={{
+          'menu-open': (orient) => ({ x: orient === 'right' ? `-${dimensions.menuWidth}` : dimensions.menuWidth }),
+          'menu-closed': { x: 0 },
+        }}
+        animate={variant}
+        transition={{ type: 'tween', duration: 0.5, ease: [0.25, 0.1, 0.25, 1] }}
+        custom={orientation}
+        initial={false}
       >
-        <div className={styles.navCover} />
-        <div className={styles.content}>{lightContent}</div>
+        {/* There's a menu off screen to the left */}
+        <Menu orientation="left" />
+
+        {/* Light panel */}
+        <motion.div
+          layoutTransition
+          className={`${styles.panel} ${styles.light}`}
+          style={{ gridColumn: {
+            left: 'viewport-left / fifth 2',
+            right: 'fifth 3 / viewport-right',
+            full: 'viewport-left / viewport-right',
+          }[orientation] }}
+        >
+          <div className={styles.navCover} />
+          <div className={styles.content}>{lightContent}</div>
+        </motion.div>
+
+        {/* Dark content sits "behind" and to the side of right panel */}
+        { orientation === 'full' ? <React.Fragment />
+          : (
+            <motion.div
+              layoutTransition
+              className={`${styles.panel} ${styles.dark}`}
+              style={{ gridColumn: {
+                left: 'fifth 2 / viewport-right',
+                right: 'viewport-left / fifth 3',
+              }[orientation] }}
+            >
+              <div className={styles.navCover} />
+              <div className={styles.content}>{darkContent}</div>
+            </motion.div>
+          ) }
+
+        {/* There's a menu off screen to the right */}
+        <Menu orientation="right" />
       </motion.div>
-
-      {/* Dark content sits "behind" and to the side of right panel */}
-      { orientation === 'full' ? <React.Fragment />
-        : (
-          <motion.div
-            layoutTransition
-            className={`${styles.panel} ${styles.dark}`}
-            style={{ gridColumn: {
-              left: 'fifth 2 / viewport-right',
-              right: 'viewport-left / fifth 3',
-            }[orientation] }}
-          >
-            <div className={styles.navCover} />
-            <div className={styles.content}>{darkContent}</div>
-          </motion.div>
-        ) }
-
-      {/* There's a menu off screen to the right */}
-      <Menu orientation="right" />
-    </motion.div>
+    </React.Fragment>
   );
 }
 
