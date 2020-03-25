8f2711c55d32058acd3e990b478c1bb5f4b39c8b
Luke Taylor <luke@deentaylor.com>
2020-03-25 03:17:48 -0400

    Display stars count correctly on hello page

--- src/pages/api/content/hello.js
+++ src/pages/api/content/hello.js
@@ -1,22 +1,45 @@
+/* eslint-disable camelcase */
+
+import fetch from 'isomorphic-unfetch';
+
 import Prismic from 'prismic-javascript';
 import PrismicDOM from 'prismic-dom';
 import initApi from '../../../helpers/prismic';
 
+import { getTotalStars } from '../stars';
+
+/** Fetch the total star count across all my GitHub repos in a way that works both client/server */
+function isomorphicGetStars() {
+  // If we can read process.env.GITHUB_TOKEN we can use it to make the request ourselves
+  if (typeof process !== 'undefined' && process?.env?.GITHUB_TOKEN) return getTotalStars();
+  // If we're client-side, we don't have the token but, we can request the API route
+  return fetch('/api/stars')
+    .then((r) => r.json())
+    .then((data) => data.totalStars);
+}
+
+
 export async function getHomepage(req) {
   const api = await initApi(req);
-  /* eslint-disable camelcase */
-  return api.query(Prismic.Predicates.at('document.type', 'homepage'))
-    .then(({ results }) => results[0].data)
-    .then(({ main_title, text }) => ({
-      title: PrismicDOM.RichText.asHtml(main_title),
-      text: text.map(({ text_content }) => PrismicDOM.RichText.asHtml(text_content)),
-    }))
-    // Special case: replace instances of the string {{age}} with a span
-    .then(({ title, text }) => ({
-      title,
-      text: text.map((t) => t.replace(/{{age}}/g, '<span class="age"></span>')),
-    }));
-  /* eslint-enable */
+
+  const [cmsData, totalGithubStars] = await Promise.all([
+    // Fetch page content from CMS
+    api.query(Prismic.Predicates.at('document.type', 'homepage'))
+      .then(({ results }) => results[0].data)
+      .then(({ main_title, text: textBlocks }) => ({
+        title: PrismicDOM.RichText.asHtml(main_title),
+        text: textBlocks.map(({ text_content }) => PrismicDOM.RichText.asHtml(text_content)),
+      })),
+    // Simultaneously, fetch my total number of GitHub stars from the API
+    isomorphicGetStars(),
+  ]);
+
+  return {
+    ...cmsData,
+    text: cmsData.text
+      .map((t) => t.replace(/{{age}}/g, '<span class="age"></span>'))
+      .map((t) => t.replace(/{{stars_count}}/g, `<span class="stat">${totalGithubStars}</span>`)),
+  };
 }
 
 export default async (req, res) => {
