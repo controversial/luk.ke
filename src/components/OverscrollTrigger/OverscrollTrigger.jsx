37f1004fdf7878e402f8d6b7623affccb70430ab
Luke Taylor <luke@deentaylor.com>
2020-03-29 03:12:53 -0400

    Add radial progress indicator for "overscroll"

--- src/components/OverscrollTrigger/OverscrollTrigger.jsx
+++ src/components/OverscrollTrigger/OverscrollTrigger.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
+import { motion, useMotionValue, useTransform, useSpring } from 'framer-motion';
 import { spring } from 'popmotion';
 import DownArrow from '../../../public/static/icons/down-arrow.svg';
 
@@ -30,6 +31,24 @@ function scrollDown() {
 }
 
 function OverscrollTrigger({ callback }) {
+  const isWindow = typeof window !== 'undefined';
+  const overscroll = useMotionValue(0);
+  const threshold = isWindow ? window.innerHeight : 0;
+  const progress = useTransform(overscroll, [0, threshold], [0, 1]);
+  const pathLength = useSpring(progress, { stiffness: 400, damping: 90 });
+
+  // Set up wheel event listener
+  useEffect(() => {
+    function onScroll(e) {
+      // If we're at the bottom of the scroll, increment "overscroll" by the amount we scrolled
+      if (isScrolledToBottom()) overscroll.set(overscroll.get() + e.deltaY);
+      // If we're not at the bottom of the scroll, record that there is no overscroll
+      else overscroll.set(0);
+    }
+    window.addEventListener('wheel', onScroll);
+    return () => window.removeEventListener('wheel', onScroll);
+  }, []);
+
   return (
     <button
       type="button"
@@ -37,11 +56,28 @@ function OverscrollTrigger({ callback }) {
       onClick={() => {
         // If we're not at the bottom, animate scroll to the bottom
         if (!isScrolledToBottom()) scrollDown();
-        // If we are at the bottom, run the callback func
-        else callback();
+        // If we are at the bottom, fill up the overscroll progress bar, and run the callback func
+        else { overscroll.set(threshold); callback(); }
       }}
     >
       <DownArrow className={cx('arrow-down')} />
+      <motion.svg className={cx('progress')} viewBox="0 0 24 24">
+        <motion.path
+          fill="none"
+          strokeWidth="2"
+          stroke="currentColor"
+          strokeDasharray="0 1"
+          d="
+            M 4,12
+            a 8,8 0 1,0 16,0
+            a 8,8 0 1,0 -16,0"
+          style={{
+            pathLength,
+            rotate: 90,
+            scaleX: -1,
+          }}
+        />
+      </motion.svg>
       Scroll down
     </button>
   );
