987161ab4886a5b60e5ebe3d0efe0afcc9e1a7bc
Luke Taylor <luke@deentaylor.com>
2020-04-16 22:29:46 -0400

    Draft definition of layout 2

--- src/components/ParallaxScroll/parallax-layouts.js
+++ src/components/ParallaxScroll/parallax-layouts.js
@@ -5,9 +5,11 @@
 // of the viewport. They reach their 'to' values as it disappears past the top of the page
 
 export default {
+  // Layout 1: large vertical primary image to the right, slightly smaller vertical image to the
+  //           top left, and a small vertical image to the bottom left of the primary image
   'layout-1': [
     {
-      from: { top: 75, zoom: 1.4, right: 0, size: [48, 52], zIndex: 2 },
+      from: { top: 75, zoom: 1.5, right: 0, size: [48, 52], zIndex: 2 },
       to: { top: 10, zoom: 1 },
     },
     {
@@ -16,6 +18,21 @@ export default {
     },
     {
       from: { top: 90, zoom: 1, left: 18, size: [29, 32], zIndex: 3 },
+    },
+  ],
+
+  'layout-2': [
+    {
+      from: { top: 45, zoom: 1.35, left: 0, size: [45, 35], zIndex: 2 },
+      to: { zoom: 1 },
+    },
+    {
+      from: { top: 10, zoom: 1, right: 0, size: [40, 35], zIndex: 1 },
+      to: { top: 40, zoom: 1.35 },
+    },
+    {
+      from: { top: 95, zoom: 1, left: 30, size: [35, 40], zIndex: 3 },
+      to: { top: 45, zoom: 1.1 },
     },
   ],
 };
