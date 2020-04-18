4ec7819c5361ed045a731a5b8ef9dfcd66a1e8b3
Luke Taylor <luke@deentaylor.com>
2020-04-18 17:20:18 -0400

    Fetch data for case study page

--- src/pages/work/[project].jsx
+++ src/pages/work/[project].jsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 
-function CaseStudy() {
-  const router = useRouter();
+// component imports here
 
+import { getProject } from '../api/content/work/[project]';
+import parse from 'html-react-parser';
+
+import styles from './[project].module.sass';
+const cx = classNames.bind(styles);
+
+
+function CaseStudy({ content }) {
   return (
-    <div className="page work-case-study">
-      <h1>
-        Work:&nbsp;
-        {router.query.project}
-      </h1>
+    <div className={cx('page')}>
+      { parse(content.head) }
     </div>
   );
 }
 
+CaseStudy.propTypes = {
+  content: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+    head: PropTypes.string.isRequired,
+    subhead: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    featured_images: PropTypes.arrayOf(PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string,
+      dimensions: PropTypes.arrayOf(PropTypes.number),
+      show_overlay: PropTypes.bool,
+    })).isRequired,
+  }).isRequired,
+};
+
+
 Object.assign(CaseStudy, {
   pageName: 'Work',
+
+  async getInitialProps(ctx) {
+    return { content: await getProject(ctx.req, ctx.query.project) };
+  },
 });
 
 export default CaseStudy;
