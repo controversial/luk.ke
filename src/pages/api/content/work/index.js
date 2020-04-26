657e31e701398cbcacbfb33c97e49da766189fc4
Luke Taylor <luke@deentaylor.com>
2020-04-26 13:13:15 -0400

    Featured images can include videos

--- src/pages/api/content/work/index.js
+++ src/pages/api/content/work/index.js
@@ -24,11 +24,12 @@ export const processProject = async ({ uid, data: project, tags }, includeConten
   end_date: project.end_date.split('-').slice(0, 2).map((n) => parseInt(n, 10)),
   github_link: PrismicDOM.Link.url(project.github_link),
   featured_images: project.featured_images
-    .map(({ image: { url, alt, dimensions }, zoom }) => ({
+    .map(({ image: { url, alt, dimensions }, video, zoom }) => ({
       src: url,
       alt,
       dimensions: [dimensions.width, dimensions.height],
       zoom,
+      ...video.url && { video: video.url },
     })),
   featured_images_layout: project.featured_images_layout,
 
