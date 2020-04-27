3cbc6b48e0b1fa525b3c1760f9fdd903e39b0451
Luke Taylor <luke@deentaylor.com>
2020-04-27 16:00:46 -0400

    Support external targets for ArrowLink

--- src/components/ArrowLink/ArrowLink.jsx
+++ src/components/ArrowLink/ArrowLink.jsx
@@ -9,6 +9,22 @@ const cx = classNames.bind(styles);
 
 
 export default function ArrowLink({ children, className, ...props }) {
+  const { href } = props;
+  const isAbsolute = (() => {
+    try { return !!new URL(href); } catch (e) { return false; }
+  })();
+
+  // Absolute (external) URLs can't be wrapped in a Link
+  if (isAbsolute) {
+    return (
+      <a className={classNames(cx('arrow-link'), className)} {...props}>
+        { children }
+        <ArrowDownIcon className={cx('arrow')} />
+      </a>
+    );
+  }
+
+  // Internal URLs need to be wrapped in a Link
   return (
     <Link {...props}>
       <a className={classNames(cx('arrow-link'), className)}>
@@ -22,7 +38,9 @@ export default function ArrowLink({ children, className, ...props }) {
 ArrowLink.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  href: PropTypes.string,
 };
 ArrowLink.defaultProps = {
   className: '',
+  href: undefined,
 };
