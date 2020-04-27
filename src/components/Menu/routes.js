25fa472f3e87b141128a602cf08e5acecf16123e
Luke Taylor <luke@deentaylor.com>
2020-04-26 20:25:51 -0400

    Begin contact page for real

--- src/components/Menu/routes.js
+++ src/components/Menu/routes.js
@@ -3,4 +3,5 @@
 export default {
   Hello: ['/'],
   Work: ['/work', '/work/[project]'],
+  Contact: ['/contact'],
 };
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-export default function ContactPage() {
-  return <div>Coming soon</div>;
+import Head from 'next/head';
+
+import styles from './index.module.sass';
+const cx = classNames.bind(styles);
+
+
+function Contact() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Luke Deen Taylor</title>
+      </Head>
+    </React.Fragment>
+  );
 }
+
+function ContactPageLightContent() {
+  return (
+    <div>Light</div>
+  );
+}
+
+function ContactPageDarkContent() {
+  return (
+    <div>Dark</div>
+  );
+}
+
+Object.assign(Contact, {
+  LightContent: ContactPageLightContent,
+  DarkContent: ContactPageDarkContent,
+  pageName: 'Contact',
+  panelOrientation: 'right',
+});
+
+
+export default Contact;
