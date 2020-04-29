cb07961e234b37f7d372fa9d67d9853cbd1c4831
Luke Taylor <luke@deentaylor.com>
2020-04-29 11:39:57 -0400

    Add webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-};
+});
