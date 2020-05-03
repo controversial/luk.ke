84befe9bf66c0ca0bbb88d8da428bd4c25165b8d
Luke Taylor <luke@deentaylor.com>
2020-05-02 20:43:41 -0400

    Add simple favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,6 +7,9 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Head>
 
         <body>
