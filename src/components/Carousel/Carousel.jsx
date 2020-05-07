ea6ba0980bda2e3bd4547d19561b3b9fc449ef77
Luke Taylor <luke@deentaylor.com>
2020-05-06 22:16:47 -0400

    Enforce unique keys for Carousel children

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -10,6 +10,13 @@ function Carousel({
   children,
   className,
 }) {
+  // Check for missing / duplicate keys on children
+  const keys = React.Children.map(children, (c) => c.key);
+  const missingKeys = keys.length !== React.Children.count(children);
+  const hasDuplicates = keys.some((val, idx, arr) => (arr.indexOf(val) !== idx));
+  if (missingKeys || hasDuplicates) {
+    throw new Error('Each child of Carousel must have a unique "key" prop');
+  }
   return (
     <div className={classNames(className, cx('main'))}>
       { children }
