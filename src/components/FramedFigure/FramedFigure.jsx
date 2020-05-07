45e77020a342d3f18bde4d212ecdd7780e5e404a
Luke Taylor <luke@deentaylor.com>
2020-05-06 23:04:05 -0400

    Support custom styles on FramedFigure

--- src/components/FramedFigure/FramedFigure.jsx
+++ src/components/FramedFigure/FramedFigure.jsx
@@ -6,11 +6,11 @@ import styles from './FramedFigure.module.sass';
 const cx = classNames.bind(styles);
 
 
-export default function FramedFigure({ children, caption, focused, frameStyle, className }) {
+export default function FramedFigure({ children, caption, focused, frameStyle, className, style }) {
   const child = React.Children.only(children);
 
   return (
-    <figure className={classNames(cx('framed-figure', { focused }), className)}>
+    <figure className={classNames(cx('framed-figure', { focused }), className)} style={style}>
       <div className={cx('frame', frameStyle)}>
         {/* Elements styled to look like "browser chrome" around the contents */}
         <div className={cx('chrome')} aria-hidden>
@@ -33,10 +33,12 @@ FramedFigure.propTypes = {
   focused: PropTypes.bool,
   frameStyle: PropTypes.oneOf(['light', 'dark', 'none']),
   className: PropTypes.string,
+  style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 };
 FramedFigure.defaultProps = {
   caption: null,
   focused: false,
   frameStyle: 'dark',
   className: undefined,
+  style: {},
 };
