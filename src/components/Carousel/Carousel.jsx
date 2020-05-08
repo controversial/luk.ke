b69aab46ec150782e219d5eb5c7bd882fd1bcf46
Luke Taylor <luke@deentaylor.com>
2020-05-07 20:27:23 -0400

    Simplify Carousel by converting children to an array

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -7,27 +7,21 @@ const cx = classNames.bind(styles);
 
 
 function Carousel({
-  children,
+  children: childrenProp,
   spacing,
   itemWidth,
   className,
 }) {
+  const children = React.Children.toArray(childrenProp);
   // Check for missing / duplicate keys on children
-  const keys = React.Children.map(children, (c) => c.key);
-  const missingKeys = keys.length !== React.Children.count(children);
+  const keys = children.map((c) => c.key);
+  const missingKeys = keys.some((key) => typeof key === 'undefined');
   const hasDuplicates = keys.some((val, idx, arr) => (arr.indexOf(val) !== idx));
-  if (missingKeys || hasDuplicates) {
-    throw new Error('Each child of Carousel must have a unique "key" prop');
-  }
+  if (missingKeys || hasDuplicates) throw new Error('Each child of Carousel must have a unique "key" prop');
 
   return (
     <div className={classNames(className, cx('main'))}>
-      {
-        React.Children.map(
-          children,
-          (c) => React.cloneElement(c, { style: { width: `${itemWidth}px` } }),
-        )
-      }
+      { children.map((c) => React.cloneElement(c, { style: { width: `${itemWidth}px` } })) }
     </div>
   );
 }
