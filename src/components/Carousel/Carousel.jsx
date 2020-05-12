f735d68ee6168db327367e753691b1ccd3947cb3
Luke Taylor <luke@deentaylor.com>
2020-05-11 21:57:40 -0400

    Handle any number of Carousel elements

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -46,23 +46,31 @@ function Carousel({
   className,
 }) {
   const children = React.Children.toArray(childrenProp);
+  // If there are no children, return just the wrapper element
+  if (!children.length) return <div className={classNames(className, cx('wrapper'))} />;
   // Check for missing / duplicate keys on children
   const keys = children.map((c) => c.key);
   const missingKeys = keys.some((key) => typeof key === 'undefined');
   const hasDuplicates = keys.some((val, idx, arr) => (arr.indexOf(val) !== idx));
   if (missingKeys || hasDuplicates) throw new Error('Each child of Carousel must have a unique "key" prop');
+  // Repeat children array until we have at least five children
+  while (children.length < 5) children.push(...children);
+  // Make sure that any child elements that we duplicated will still have unique keys if rendered
+  // multiple times
+  const children2 = children.map((c, i) => React.cloneElement(c, { key: `${c.key}@${i}` })); // eslint-disable-line react/no-array-index-key
+  // Invariant at this point: `children` has five or more elements
 
   // Initially, the first element is selected
   const [currentKey, setCurrentKey] = useState(keys[0]);
 
   // What index in the children array is the item that is currently centered?
-  const centerIndex = children.findIndex((c2) => c2.key === currentKey);
+  const centerIndex = children2.findIndex((c2) => c2.key === currentKey);
   // We render two elements on either side of the center element, no matter how big the array is.
   // This array contains the indices, in the five-element-minimum `children` array, of those els.
   const indices = [centerIndex - 2, centerIndex - 1, centerIndex, centerIndex + 1, centerIndex + 2]
-    .map((i) => i + children.length) // guarantee positive values
-    .map((i) => i % children.length); // wrap around
-  const renderChildren = indices.map((i) => children[i]);
+    .map((i) => i + children2.length) // guarantee positive values
+    .map((i) => i % children2.length); // wrap around
+  const renderChildren = indices.map((i) => children2[i]);
 
   const totalWidth = (itemWidth * indices.length) + (spacing * indices.length - 1);
 
