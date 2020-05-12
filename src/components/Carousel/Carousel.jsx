e0e5da24cd451506c366b729137aec80d37bdd4c
Luke Taylor <luke@deentaylor.com>
2020-05-11 21:55:24 -0400

    Render carousel items properly side by side

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -10,13 +10,17 @@ const cx = classNames.bind(styles);
 
 function CarouselItem({
   deltaFromCenter,
+  style,
   setCurrent,
   children,
+  key,
 }) {
   return (
     <motion.div
       className={cx('item', { selected: deltaFromCenter === 0 })}
       onClick={setCurrent}
+      style={{ ...style }}
+      key={key}
     >
       { children }
     </motion.div>
@@ -25,6 +29,8 @@ function CarouselItem({
 CarouselItem.propTypes = {
   deltaFromCenter: PropTypes.number.isRequired,
   setCurrent: PropTypes.func,
+  style: PropTypes.shape({ width: Number, x: Number }).isRequired,
+  key: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
 CarouselItem.defaultProps = {
@@ -45,22 +51,38 @@ function Carousel({
   const hasDuplicates = keys.some((val, idx, arr) => (arr.indexOf(val) !== idx));
   if (missingKeys || hasDuplicates) throw new Error('Each child of Carousel must have a unique "key" prop');
 
+  // Initially, the first element is selected
   const [currentKey, setCurrentKey] = useState(keys[0]);
 
+  // What index in the children array is the item that is currently centered?
+  const centerIndex = children.findIndex((c2) => c2.key === currentKey);
+  // We render two elements on either side of the center element, no matter how big the array is.
+  // This array contains the indices, in the five-element-minimum `children` array, of those els.
+  const indices = [centerIndex - 2, centerIndex - 1, centerIndex, centerIndex + 1, centerIndex + 2]
+    .map((i) => i + children.length) // guarantee positive values
+    .map((i) => i % children.length); // wrap around
+  const renderChildren = indices.map((i) => children[i]);
+
+  const totalWidth = (itemWidth * indices.length) + (spacing * indices.length - 1);
+
   return (
-    <div className={classNames(className, cx('main'))}>
+    <div
+      className={classNames(className, cx('main'))}
+      style={{ width: totalWidth }}
+    >
       {
-        children.map((c) => {
-          const centerIndex = children.findIndex((c2) => c2.key === currentKey);
-          const deltaFromCenter = children.indexOf(c) - centerIndex;
+        renderChildren.map((child, index) => {
+          // the center element is at position 2 and should have 'deltaFromCenter' of 0
+          const deltaFromCenter = index - 2;
 
           return (
             <CarouselItem
-              key={c.key}
+              key={child.key}
               deltaFromCenter={deltaFromCenter}
-              setCurrent={() => setCurrentKey(c.key)}
+              style={{ width: itemWidth }}
+              setCurrent={() => setCurrentKey(child.key)}
             >
-              { c }
+              { React.cloneElement(child) }
             </CarouselItem>
           );
         })
