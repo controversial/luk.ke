61f69fe4bd0ed35602e41116e86a95d61cbce436
Luke Taylor <luke@deentaylor.com>
2020-05-23 14:26:52 -0400

    Enable absolute SASS imports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')],
+  },
 });
