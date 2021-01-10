4900fa6aef8be12426bc88e51aafdf1e0aea1cfd
Luke Taylor <luke@deentaylor.com>
2021-01-09 20:41:01 -0500

    Add a (very dirty) babel layer in sequences.js

--- src/components/MobileLayout/sequences.js
+++ src/components/MobileLayout/sequences.js
@@ -3,12 +3,51 @@
  *       function resolves to. This system should be moved to getStaticProps if next.js#10949 is
  *       resolved. */
 
-const { getProjects } = require('../../pages/api/content/work/index.js');
+// This includes a lot of hacks for compatibility with Node 12
+// All the babel stuff should be replaced by a normal require once Node 14+ ships on Vercel
+
+const nextBabelLoader = require('next/dist/build/webpack/loaders/next-babel-loader');
+const fs = require('fs').promises;
+const path = require('path');
+
+const Module = require('module');
+
+
+async function babelRequire(modulePath) {
+  const abspath = require.resolve(modulePath);
+  const moduleCode = await fs.readFile(abspath, 'utf-8');
+
+  // Hack together something that looks enough like a Webpack interface that it will run the loader
+  const babelifiedModuleCode = await new Promise((resolve, reject) => {
+    const webpack = {
+      async: () => (err, val) => {
+        if (err) reject(err);
+        else resolve(val);
+      },
+      query: { cwd: __dirname, babelPresetPlugins: [] },
+      resourcePath: abspath,
+      emitWarning: console.warn,
+      addDependency: () => {},
+      compile: nextBabelLoader,
+    };
+    webpack.compile(moduleCode);
+  });
+  const newModule = new Module(abspath, module);
+  newModule.paths = Module._nodeModulePaths(path.dirname(abspath));
+  newModule.filename = abspath;
+  newModule._compile(babelifiedModuleCode, abspath);
+  return newModule.exports;
+}
+
+// const { getProjects } = require('../../pages/api/content/work/index.js');
 
 // Sequences are ordered collections of pages.
 // In the portrait “mobile” layout, they define the order in which pages are laid out horizontally.
 
 module.exports = async function getMobileRouteSequences() {
+  const workContentModulePath = '../../pages/api/content/work/index.js';
+  const { getProjects } = await babelRequire(workContentModulePath);
+
   const sequences = [
     // Default sequence of “main” pages
     [
@@ -26,6 +65,11 @@ module.exports = async function getMobileRouteSequences() {
 
   return {
     code: `module.exports = ${JSON.stringify(sequences)};`,
-    dependencies: [require.resolve('../../pages/api/content/work/index.js')],
+    dependencies: [require.resolve(workContentModulePath)],
   };
 };
+
+// Allow running with 'node sequences.js' in testing
+if (!module.parent) {
+  module.exports().then(console.log);
+}
