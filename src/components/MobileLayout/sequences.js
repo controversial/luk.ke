ff92a83af00cd1e7242e2136a3517630483c04e9
Luke Deen Taylor <luke@deentaylor.com>
2021-04-29 20:08:59 +0000

    Update for Node 14 but less
    At least skip the transpilation mess even if we can't avoid commonjs

--- src/components/MobileLayout/sequences.js
+++ src/components/MobileLayout/sequences.js
@@ -6,47 +6,13 @@
 // This includes a lot of hacks for compatibility with Node 12
 // All the babel stuff should be replaced by a normal require once Node 14+ ships on Vercel
 
-const nextBabelLoader = require('next/dist/build/webpack/loaders/next-babel-loader');
-const fs = require('fs').promises;
-const path = require('path');
-
-const Module = require('module');
-
-
-async function babelRequire(modulePath) {
-  const abspath = require.resolve(modulePath);
-  const moduleCode = await fs.readFile(abspath, 'utf-8');
-
-  // Hack together something that looks enough like a Webpack interface that it will run the loader
-  const babelifiedModuleCode = await new Promise((resolve, reject) => {
-    const webpack = {
-      async: () => (err, val) => {
-        if (err) reject(err);
-        else resolve(val);
-      },
-      query: { cwd: __dirname, babelPresetPlugins: [] },
-      resourcePath: abspath,
-      emitWarning: console.warn,
-      addDependency: () => {},
-      compile: nextBabelLoader,
-    };
-    webpack.compile(moduleCode);
-  });
-  const newModule = new Module(abspath, module);
-  newModule.paths = Module._nodeModulePaths(path.dirname(abspath));
-  newModule.filename = abspath;
-  newModule._compile(babelifiedModuleCode, abspath);
-  return newModule.exports;
-}
-
-// const { getProjects } = require('../../pages/api/content/work/index.js');
+const { getProjects } = require('../../pages/api/content/work/index.js');
 
 // Sequences are ordered collections of pages.
 // In the portrait “mobile” layout, they define the order in which pages are laid out horizontally.
 
 module.exports = async function getMobileRouteSequences() {
   const workContentModulePath = '../../pages/api/content/work/index.js';
-  const { getProjects } = await babelRequire(workContentModulePath);
 
   const sequences = [
     // Default sequence of “main” pages
