946804ac1be5c0613dcb763e75b22793bebc2d6a
Luke Taylor <luke@deentaylor.com>
2021-05-14 01:32:37 -0400

    Fix node 14 compat

--- src/helpers/server/screenshot.js
+++ src/helpers/server/screenshot.js
@@ -1,13 +1,13 @@
 import puppeteer from 'puppeteer';
 import chr from 'chrome-aws-lambda';
-import { setTimeout } from 'timers/promises';
+import { promisify } from 'util';
 
 const MAC_BINARY_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 async function getChromeLaunchOptions() {
   return process.env.VERCEL
     ? { executablePath: await chr.executablePath, args: chr.args, headless: chr.headless }
-    : { executablePath: MAC_BINARY_PATH, headless: false };
+    : { executablePath: MAC_BINARY_PATH };
 }
 
 export default async function screenshot(url, scale = 1, viewport = { width: 1200, height: 627 }) {
@@ -17,7 +17,7 @@ export default async function screenshot(url, scale = 1, viewport = { width: 120
   try {
     await page.goto(url, { waitUntil: 'networkidle0', timeout: 5000 });
   } catch (e) {}
-  await setTimeout(250);
+  await promisify(setTimeout)(250);
   const file = await page.screenshot();
   await browser.close();
   return file;
